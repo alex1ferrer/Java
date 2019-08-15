b093aaba7d95b4c279d6c91b63d838d035d48a8c
alex1ferrer <alejandroferrerrodriguezz@gmail.com>
2019-08-15 22:38:31 +0200

    se implementa ejercicio de calcular notas

--- src/ejercicio01/Main.java
+++ src/ejercicio01/Main.java
@@ -1,10 +1,37 @@
 package ejercicio01;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("okdevelop");
-		System.out.println("ok");
+		int nota_tema2, nota_tema4, nota_diciembre;
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Introduce la nota de la práctica 2: ");
+		nota_tema2 = scanner.nextInt();
+		System.out.print("Introduce la nota de la práctica 4: ");
+		nota_tema4 = scanner.nextInt();
+		System.out.print("Introduce la nota del examen: ");
+		nota_diciembre = scanner.nextInt();
+
+		if (nota_tema2 >= 3 && nota_tema4 >= 3 && media(nota_tema2, nota_tema4) >= 4 && nota_diciembre >= 4) {
+			System.out.println("Puedes aprobar la evaluación");
+		} else if (nota_tema2 < 3 || nota_tema4 < 3 || media(nota_tema2, nota_tema4) < 4 || nota_diciembre >= 4) {
+			System.out.println("Debes recuperar las prácticas");
+		} else if (nota_diciembre < 4 && nota_tema2 >= 3 && nota_tema4 >= 3 && media(nota_tema2, nota_tema4) >= 4) {
+			System.out.println("Debes recuperar el examen");
+		} else {
+			System.out.println("Debes recuperarlo todo");
+		}
+	}
+
+	public static int suma(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public static int media(int num1, int num2) {
+		return suma(num1, num2) / 2;
 	}
 
 }
